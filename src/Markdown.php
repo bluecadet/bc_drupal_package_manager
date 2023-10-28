5fd8f3da0a30604346cdd57188ac2acbd414c695
Pete Inge <pinge@bluecadet.com>
2023-10-28 09:54:48 -0400

    Add in some more styling and summary info

--- src/Markdown.php
+++ src/Markdown.php
@@ -7,6 +7,8 @@ use PHP_CodeSniffer\Reports\Report;
 
 class Markdown implements Report {
 
+    protected $colors = FALSE;
+
     /**
      * Generate a partial report for a single processed file.
      *
@@ -23,18 +25,17 @@ class Markdown implements Report {
      */
     public function generateFileReport($report, File $phpcsFile, $showSources=false, $width=80) {
 
-      $colors = $phpcsFile->config->__get('colors');
+      $this->colors = $colors = $phpcsFile->config->__get('colors');
 
       if (!empty($report['messages'])) {
 
         echo "zzzzzz" . $report['filename'].'>>'.$report['errors'].'>>'.$report['warnings'] . "zzzzzz" . PHP_EOL;
 
-
-        echo "___" . PHP_EOL;
-        echo "## FILE: " . $report['filename'] . PHP_EOL;
+        // echo "___" . PHP_EOL;
+        echo "### FILE: " . $report['filename'] . PHP_EOL;
         echo "" . PHP_EOL;
 
-        echo "### FOUND ";
+        echo "#### FOUND ";
         if ($report['errors'] > 0) {
           echo $report['errors'] . " ERRORS ";
         }
@@ -45,15 +46,15 @@ class Markdown implements Report {
 
 
         echo "".PHP_EOL;
-        echo "| Line # | Severity | FIX | Message |" . PHP_EOL;
-        echo "| -----: | :------: | :-: | :------ |" . PHP_EOL;
+        echo "| Line # | Type | Severity | FIX | Message |" . PHP_EOL;
+        echo "| -----: | :--: | :------: | :-: | :------ |" . PHP_EOL;
 
         foreach ($report['messages'] as $line => $lineErrors) {
           foreach ($lineErrors as $column => $colErrors) {
             foreach ($colErrors as $error) {
               echo "| " . $line . " | ";
               echo $colors? "<span style=\"color: " . (($error['type'] == "ERROR")? "red" : "yellow") . "\">" . $error['type'] . "</span>" : $error['type'];
-              echo " | ";
+              echo " | " . $error['severity'] . " | ";
               echo ($error['fixable'])? "[x]" : "[ ]";
               echo " | " . $error['message'];
               if ($showSources) echo "<br>&nbsp;&nbsp;(" . $error['source'] . ")";
@@ -66,7 +67,10 @@ class Markdown implements Report {
         echo "".PHP_EOL;
         echo "<br><br>".PHP_EOL;
         echo "".PHP_EOL;
+
+        return TRUE;
       }
+
     }
 
     /**
@@ -100,21 +104,51 @@ class Markdown implements Report {
             return;
         }
 
-        $cachedData = preg_replace('/zzzzzz.*zzzzzz/i', "", $cachedData);
-
         echo "# PHPCS Report" . PHP_EOL;
         echo "" . PHP_EOL;
 
+        echo "## Summary" . PHP_EOL;
+        echo "" . PHP_EOL;
 
-        echo "Total Errors: " . $totalErrors . PHP_EOL;
+        preg_match_all('/zzzzzz(.*)>>(.*)>>(.*)zzzzzz/i', $cachedData, $matches);
+
+        $cachedData = preg_replace('/zzzzzz.*zzzzzz/i', "", $cachedData);
+        $data = [];
+        foreach ($matches[0] as $i => $match_data) {
+          $data[] = [
+            $matches[1][$i],
+            $matches[2][$i],
+            $matches[3][$i],
+          ];
+        }
+
+        echo "| FILE | ERRORS | WARNINGS |" . PHP_EOL;
+        echo "| :--- | :----: | :------: |" . PHP_EOL;
+
+        foreach ($data as $d) {
+          echo "| " . $d[0] . " | ";
+          echo $this->colors ? "<span style=\"color: red\">" . $d[1] . "</span>" : $d[1];
+          echo " | ";
+          echo $this->colors ? "<span style=\"color: yellow\">" . $d[2] . "</span>" : $d[2];
+          echo " |" . PHP_EOL;
+        }
+
+        echo "" . PHP_EOL;
+
+        echo "A TOTAL OF " . $totalErrors . " ERROR";
+        if ($totalErrors > 1) echo "S";
+        echo " AND " . $totalWarnings . " WARNING";
+        if ($totalWarnings > 1) echo "S";
+        echo " WERE FOUND IN " . $totalFiles . " FILE";
+        if ($totalFiles > 1) echo "S";
+        echo "<br>";
+
+        echo "TOTAL FIXABLE: " . $totalFixable . PHP_EOL;
         echo "" . PHP_EOL;
 
 
-        echo "Total Warnings: " . $totalWarnings . PHP_EOL;
-        echo "" . PHP_EOL;
 
-
-        echo "Total Fixable: " . $totalFixable . PHP_EOL;
+        echo "## Details" . PHP_EOL;
         echo "" . PHP_EOL;
 
         echo $cachedData;
