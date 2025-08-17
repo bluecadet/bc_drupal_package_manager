07ce4dba8402eec94955fbb45cca340d8cbb2738
Pete Inge <pete.inge@gmail.com>
2025-08-17 13:22:31 -0400

    oops: Commented out the wrong if statement.

--- src/Checker.php
+++ src/Checker.php
@@ -95,19 +95,19 @@ class Checker {
                     // Is is also?
                     if (Version::lessThan($exisiting_version, $release_version)) {
 
-                      if (!$release_version->isPreRelease()) {
+                      // I want to see all versions higher than current regardless.
+                      // if (!$release_version->isPreRelease()) {
                         $this->also[$user][$module_name][$release_version->getMajor() . "." . $release_version->getMinor()] = $package_data['version'];
-                      }
+                      // }
 
                       // Is it latest?
                       // Is it recommended?
                       if ($exisiting_version->getMajor() == $release_version->getMajor()) {
 
                         $constraint = Constraint::parse("^" . $exisiting_version->__toString());
-                        // I want to see all versions higher than current regardless.
-                        // if (!$release_version->isPreRelease()) {
+                        if (!$release_version->isPreRelease()) {
                           $this->statuses[$user][$module_name] = UpdateManagerInterface::NOT_CURRENT;
-                        // }
+                        }
 
                         if ($release_version->isPreRelease() && $exisiting_version->getMinor() == $release_version->getMinor())  {
                           $this->also[$user][$module_name][$release_version->getMajor() . "." . $release_version->getMinor() . ".x"] = $package_data['version'];
